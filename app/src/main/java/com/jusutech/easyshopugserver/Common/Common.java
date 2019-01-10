0fc6bd04150eeb669794f7e155a17bea508d9cae
Junior Joseph <juniorjoe567@gmail.com>
2019-01-10 22:46:40 +0300

    Initial commit

---
+++ app/src/main/java/com/jusutech/easyshopugserver/Common/Common.java
@@ -0,0 +1,13 @@
+package com.jusutech.easyshopugserver.Common;
+
+import com.jusutech.easyshopugserver.Model.User;
+
+/**
+ * Created by Junior Joseph on 1/8/2019.
+ */
+
+public class Common {
+    public static User currentUser;
+    public static final String UPDATE = "Update";
+    public static final String DELETE = "Delete";
+}
---
+++ app/src/main/java/com/jusutech/easyshopugserver/MainActivity.java
@@ -0,0 +1,36 @@
+package com.jusutech.easyshopugserver;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.jusutech.easyshopugserver.Model.Category;
+import com.jusutech.easyshopugserver.ViewHolder.MenuViewHolder;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button signIn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        signIn = (Button)findViewById(R.id.signin);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SignIn.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
