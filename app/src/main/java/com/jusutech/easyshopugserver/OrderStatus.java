b3ff6afe2255dffd42f21c8653ac7b1bbc9c82e9
Junior Joseph <juniorjoe567@gmail.com>
2019-01-17 11:48:37 +0300

    Added getting order details. You can now update the status of the placed order if being shipped or after it has been shipped.

--- app/src/main/java/com/jusutech/easyshopugserver/OrderStatus.java
+++ app/src/main/java/com/jusutech/easyshopugserver/OrderStatus.java
@@ -1,11 +1,15 @@
 package com.jusutech.easyshopugserver;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Spinner;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
@@ -14,6 +18,7 @@ import com.jusutech.easyshopugserver.Common.Common;
 import com.jusutech.easyshopugserver.Interface.ItemClickListener;
 import com.jusutech.easyshopugserver.Model.Request;
 import com.jusutech.easyshopugserver.ViewHolder.OrderViewHolder;
+
 
 public class OrderStatus extends AppCompatActivity {
 
@@ -24,6 +29,7 @@ public class OrderStatus extends AppCompatActivity {
     FirebaseRecyclerAdapter<Request,OrderViewHolder> adapter;
     FirebaseDatabase db;
     DatabaseReference requests;
+    Spinner spinner;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -70,17 +76,45 @@ public class OrderStatus extends AppCompatActivity {
 
     @Override
     public boolean onContextItemSelected(MenuItem item) {
-        if(item.getIntent().equals(Common.UPDATE))
+        if(item.getTitle().equals(Common.UPDATE))
             ShowUpdateDialog(adapter.getRef(item.getOrder()).getKey(), adapter.getItem(item.getOrder()));
-        else if(item.getIntent().equals(Common.UPDATE))
+        else if(item.getTitle().equals(Common.DELETE))
             deleteOrder(adapter.getRef(item.getOrder()).getKey());
         return super.onContextItemSelected(item);
     }
 
     private void deleteOrder(String key) {
+        requests.child(key).removeValue();
     }
 
-    private void ShowUpdateDialog(String key, Request item) {
-        
+    private void ShowUpdateDialog(String key, final Request item) {
+        final AlertDialog.Builder alertDialog = new AlertDialog.Builder(OrderStatus.this);
+        alertDialog.setTitle("Update Order");
+        alertDialog.setMessage("Please select the status");
+
+        LayoutInflater inflater = this.getLayoutInflater();
+        final View view = inflater.inflate(R.layout.update_order_layout,null);
+
+        spinner = (Spinner)view.findViewById(R.id.statusSpinner);
+
+        alertDialog.setView(view);
+
+        final String localKey = key;
+        alertDialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+               dialogInterface.dismiss();
+               item.setStatus(String.valueOf(spinner.getSelectedItemId()));
+
+               requests.child(localKey).setValue(item);
+            }
+        });
+        alertDialog.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        alertDialog.show();
     }
 }
